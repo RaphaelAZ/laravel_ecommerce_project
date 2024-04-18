7beba93b240d67a639a8277d831c7793f47dbe3e
RaphaelAZ <azevedo.raphael2017@gmail.com>
2024-04-18 12:33:20 +0200

    (~) refacto route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.template')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Panel') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Vous êtes connecté!') }}
                 </div>
             </div>
         </div>
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,7 +22,7 @@
 </head>
 <body>
     <div id="app">
-        @include('navbar');
+        @include('layout.navbar')
         <main class="py-4">
             @yield('content')
         </main>
