b33a897793944f63aa4a9f8d570f9b2259356b25
ViartFelix <viart.felix2017@gmail.com>
2024-05-16 15:04:54 +0200

    (~) hotfix admin navbar

--- resources/views/layouts/admin/admin-navbar.blade.php
+++ resources/views/layouts/admin/admin-navbar.blade.php
@@ -2,7 +2,7 @@
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
 @stop
 
-<nav class="bg-sec py-2 text-white">
+<nav class="bg-gray-800 py-2 text-white">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <div class="gap-9 flex items-center">
             <a class="block h-fit" href="{{ route('dashboard') }}">
